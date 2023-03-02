4c5c6160e448cae5289d85fe5c939cdaec7ebf68
ShaggyBa <71100001+ShaggyBa@users.noreply.github.com>
2023-03-02 12:34:16 +0300

    Созданы сцены: 1. MC (Spaceship) # Добавлена функция движения 2. World # Для отладки движения

---
+++ src/actors/MC/MC.gd
@@ -0,0 +1,46 @@
+extends KinematicBody2D
+
+export (float) var mcSpeed = 100.0 # cкорость полета корабля
+export (float) var mcVSpeed = 200.0 # cкорость вертикального движения корабля
+
+
+var mcMotion = Vector2() # вектор скорости
+var viewportSize : Vector2
+
+
+func _ready() -> void:
+	viewportSize = get_viewport().size # Получение размеров viewport-а
+
+func _process(delta) -> void:
+	spaceshipMove(delta) # функция движения корабля
+	
+	
+func spaceshipMove(delta) -> void:
+		
+	var inputVector = Vector2.ZERO # Обнуление координат вектора скоростей
+	
+	if Input.is_action_pressed("ui_up"): # Зажатие стрелки вверх или W
+		inputVector.y -= 1
+	
+	if Input.is_action_pressed("ui_down"): # Зажатие стрелки вниз или S
+		inputVector.y += 1
+	
+	
+	if inputVector.length() > 0: # Если есть движение корабля
+		inputVector = inputVector.normalized() * mcVSpeed
+		
+	
+	position.y += inputVector.y * delta 
+	# delta позволяет естественно набирать скорость (не моментально)	
+	position.x += mcSpeed * delta
+	position.y = clamp(position.y, 0, viewportSize.y) 
+		
+	mcMotion = move_and_slide(inputVector)
+
+
+
+
+
+
+
+
