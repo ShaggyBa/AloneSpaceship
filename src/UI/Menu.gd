5c8bbb2ec62e5252c5b8b28c6684dd37cf5d4c6e
pashazuashe <53072017+pashazuashe@users.noreply.github.com>
2023-03-07 20:10:28 +0300

    main menu, pause menu

---
+++ src/UI/Menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_StartBtn_pressed():
+	get_tree().change_scene("res://src/scenes/World.tscn")
+
+
+func _on_QuitBtn_pressed():
+	get_tree().quit()
---
+++ src/UI/PauseMenu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		self.is_paused = !is_paused
+
+
+var is_paused = false setget set_is_paused
+
+func set_is_paused(value):
+	is_paused = value
+	get_tree().paused = is_paused
+	visible = is_paused
+
+
+func _on_ResumeBtn_pressed():
+	self.is_paused = false
+
+
+func _on_QuitBtn_pressed():
+	get_tree().change_scene("res://src/UI/Menu.tscn")
+
+
+func _on_PauseBtn_pressed():
+	self.is_paused = !is_paused
