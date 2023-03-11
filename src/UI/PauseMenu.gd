cc9470c330359242be78d4ea11eb11527a0bd605
Pasha Kirillov <89199389177lol@gmail.com>
2023-03-11 18:54:34 +0300

    Добавил счетики, исправил баг при пере запуске игры

--- src/UI/PauseMenu.gd
+++ src/UI/PauseMenu.gd
@@ -1,11 +1,15 @@
 extends Control
 
+var is_paused = false setget set_is_paused
+
+func _ready():
+	self.is_paused = !is_paused
+	self.is_paused = !is_paused
+
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
 		self.is_paused = !is_paused
 
-
-var is_paused = false setget set_is_paused
 
 func set_is_paused(value):
 	is_paused = value
@@ -23,3 +27,5 @@ func _on_QuitBtn_pressed():
 
 func _on_PauseBtn_pressed():
 	self.is_paused = !is_paused
+
+
