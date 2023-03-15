dce623363fcf011daf5370c60db48e8a5315f796
ShaggyBa <71100001+ShaggyBa@users.noreply.github.com>
2023-03-15 21:30:30 +0300

    Время коммитить
    Улучшение поведения врагов

--- src/scenes/World.gd
+++ src/scenes/World.gd
@@ -6,5 +6,5 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	$MeteoriteSpawner.global_position.x = $MC.global_position.x + 1000
+	$MeteoriteSpawner.global_position.x += $MC.mcVSpeed * delta
 
