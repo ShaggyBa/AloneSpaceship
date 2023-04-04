94847f3a8cae51d659259f4a9a72c42b51639ad2
ShaggyBa <71100001+ShaggyBa@users.noreply.github.com>
2023-04-04 10:50:07 +0300

    add boss spawn (test)

--- src/actors/Enemies/EnemySpawner.gd
+++ src/actors/Enemies/EnemySpawner.gd
@@ -4,6 +4,9 @@ extends Node2D
 export (float) var nextSpawnTime = 5.0
 export (float) var minSpawnRate = 1.0
 export (int) var maxEnemySpawn = 5
+export (bool) var bossSpawning = true
+export (float) var nextBossSpawn = 5
+
 
 var enemies = []
 
@@ -16,10 +19,6 @@ var preloadedEnemies = [
 var pBoss = preload("res://src/actors/Enemies/BattleEnemy/BattleEnemy.tscn")
 
 
-export (bool) var bossSpawning = true
-export (float) var nextBossSpawn = 2
-
-
 onready var spawnTimer = $SpawnTimer
 onready var bossTimer = $BossTimer
 
@@ -28,28 +27,38 @@ onready var viewportRect = get_viewport_rect()
 func _ready():
 	randomize()
 	spawnTimer.start(nextSpawnTime)
-
+	if bossSpawning:
+		bossTimer.start(nextBossSpawn)
+		print("Timer start")
+	else:
+		print("Timer delete")		
+		bossTimer.queue_free()
 	
+	
+func _process(delta):
+	print(bossTimer.time_left)
+	
+	
+func _on_BossTimer_timeout():
+	if bossTimer.is_stopped() and not get_tree().current_scene.get_node("BattleEnemy"):
+			var boss = pBoss.instance()
+			boss.global_position = Vector2($Position2D.global_position.x, viewportRect.end.y / 2)
+			get_tree().current_scene.add_child(boss)
+			bossTimer.start(nextBossSpawn)
+
 
 func _on_SpawnTimer_timeout():
+	
 	if get_tree().get_nodes_in_group("enemy").size() < maxEnemySpawn:
 		
 		# Создание врага
 		var preloadedEnemy = preloadedEnemies[randi() % preloadedEnemies.size()]
 		var enemy = preloadedEnemy.instance()
 		
-		
-		if bossSpawning and bossTimer.is_stopped():
-			var boss = pBoss.instance()
-			boss.global_position = Vector2($Position2D.global_position.x, viewportRect.end.y / 2)
-			get_tree().current_scene.add_child(boss)
-			print("add boss")
-			bossTimer.start(nextBossSpawn)
 			
 		if preloadedEnemy == preloadedEnemies[1]:
 			var enemyAttackDelay = enemy.enemyAttackDelay
 			enemy.enemyAttackDelay = rand_range(enemyAttackDelay - 0.05, enemyAttackDelay + 0.25)	
-		
 		enemy.global_position = Vector2($Position2D.global_position.x, rand_range(25, viewportRect.end.y - 25))
 		
 		if preloadedEnemy == preloadedEnemies[0]:
--- src/scenes/World.gd
+++ src/scenes/World.gd
@@ -9,8 +9,6 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	#$MeteoriteSpawner.global_position.x += $MC.mcSpeed * delta
-	#$EnemySpawner.global_position.x += $MC.mcSpeed * delta
 	points += delta * 25
 	counter.set_points(floor(points))
 	
