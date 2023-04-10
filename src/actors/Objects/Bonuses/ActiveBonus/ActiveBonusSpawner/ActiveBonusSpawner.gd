61441880814ba6a80fd2756f23285e56a32050bc
NWWN <maksimum1337@yandex.ru>
2023-04-10 22:03:35 +0300

    passive

---
+++ src/actors/Objects/Bonuses/ActiveBonus/ActiveBonusSpawner/ActiveBonusSpawner.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var preloadedBonuses = [
+		preload("res://src/actors/Objects/Bonuses/ActiveBonus/BonusShield/ShieldBonus.tscn"),
+		preload("res://src/actors/Objects/Bonuses/ActiveBonus/BonusHealth/HealthBonus.tscn"),
+		preload("res://src/actors/Objects/Bonuses/ActiveBonus/BonusDamage/DamageBonus.tscn"),
+	]
+
+export (float) var maxBonusSpawn = 1
+var nextSpawnTime = randi() % 5 + 7
+
+onready var spawnTimer = $SpawnTimer
+onready var viewportRect = get_viewport_rect()
+
+func _ready():
+	randomize()
+	spawnTimer.start(nextSpawnTime)
+
+func _on_SpawnTimer_timeout():
+	if get_tree().get_nodes_in_group("ActiveBonus").size() < maxBonusSpawn:
+		
+		var bonusPreloaded = preloadedBonuses[randi() % preloadedBonuses.size()]
+		var bonus = bonusPreloaded.instance()
+		
+		# Position 
+		bonus.position = Vector2($Position2D.global_position.x + 50, rand_range(30, viewportRect.end.y - 30))
+		get_tree().current_scene.add_child(bonus)
+	
+		# Restart timer
+		spawnTimer.start(nextSpawnTime)
+	else: 
+		spawnTimer.start(nextSpawnTime)
+		spawnTimer.start(nextSpawnTime)
+
---
+++ src/actors/Objects/Bonuses/PassiveBonus/PassiveBonusSpawner/PassiveBonusSpawner.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+export (float) var nextSpawnTime = 1
+export (int) var maxBonusSpawn = 3
+
+onready var preloadedBonuses = [
+		preload("res://src/actors/Objects/Bonuses/PassiveBonus/passiveDamage/PassiveDamage.tscn"),
+		preload("res://src/actors/Objects/Bonuses/PassiveBonus/passiveHP/PassiveHP.tscn"),
+		#preload("res://src/actors/Objects/Bonuses/PassiveBonuses/passiveShootDelay/passiveShootDelay.tscn"),
+		#preload("res://src/actors/Objects/Bonuses/PassiveBonuses/passiveMulti/passiveMulti.tscn"),
+		#preload("res://src/actors/Objects/Bonuses/PassiveBonuses/passiveSpeed/passiveSpeed.tscn")		
+	]
+
+
+onready var spawnTimer = $SpawnTimer
+onready var viewportRect = get_viewport_rect()
+
+func _ready():
+	randomize()
+	spawnTimer.start(nextSpawnTime)
+
+func _on_SpawnTimer_timeout():
+	
+	if get_tree().get_nodes_in_group("PassiveBonus").size() < maxBonusSpawn:
+	
+	# Spawn bonus
+		var bonusPreloaded = preloadedBonuses[randi() % preloadedBonuses.size()]
+		var bonus = bonusPreloaded.instance()
+		
+
+		# Position 
+		bonus.position = Vector2($Position2D.global_position.x + 50, rand_range(0, viewportRect.end.y))
+		get_tree().current_scene.add_child(bonus)
+
+		# Restart timer
+		spawnTimer.start(nextSpawnTime)
+	else: 
+		spawnTimer.start(nextSpawnTime)
+	
